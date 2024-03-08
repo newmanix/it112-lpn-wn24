7ca4343c38b9d32d690fccf0ab1f9dede3c23db4
Bill Newman <william.newman@seattlecolleges.edu>
2024-03-08 06:35:05 +0000

    Updates URLs

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("",views.home, name="home"),
-  path("about/",views.about, name="about"),
-  path('function/', views.function_view, name='function_view'),
+  path("",views.HomeView.as_view(), name="home"),
+  path("about/",views.AboutView.as_view(), name="about"),
   path('class/', views.ClassView.as_view(), name='class_view'),
   path('theme/', views.ThemeView.as_view(), name='theme'),
 ]
+
